9525d6a879c166e1bcd3817ec0d0205c759de814
Onos Ejoor <156698969+onosejoor@users.noreply.github.com>
2025-10-10 02:30:55 +0100

    fix image error

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   images: {
-    remotePatterns: [new URL("https://res.cloudinary.com/**")]
+    remotePatterns: [
+      new URL("https://res.cloudinary.com/**"),
+      new URL("https://lh3.googleusercontent.com/**"),
+    ],
   },
   /* config options here */
   async rewrites() {
